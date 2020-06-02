1075351fde1d8e225981cc4fea93a9ee5e57b1a4
Robin <robinspots@gmail.com>
2020-06-02 18:02:24 +0100

    added instructions comment

--- NorthKoreaMissileChallenge/Program.cs
+++ NorthKoreaMissileChallenge/Program.cs
@@ -4,7 +4,15 @@ namespace NorthKoreaMissileChallenge
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Methods you will need:
+        ///
+        /// Missile.MissilesInFlight // A list of the missiles in flight. Will return null if the missile isn't responding to our calls.
+        /// Missile.GetRangeInKM() // Returns a missile's remaining range in kilometres.
+        /// Missile.SetTarget(string CityName); // Sets the missile's new target. Takes a city name as the only parameter.
+        /// 
+        /// </summary>
+        static void Main()
         {
             Console.WriteLine("Greetings, Supreme Leader!");
             // TYPE YOUR CODE HERE
